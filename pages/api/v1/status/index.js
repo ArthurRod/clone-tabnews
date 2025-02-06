9c1ae5388a1e7da1e5917c4014fbfc151b5a7943
Arthur Rodrigues <arthur9713@hotmail.com>
2025-02-06 17:11:59 -0300

    chore: add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(req, res) {
-  res.status(200).json({ CR7: "SIUU" });
+import database from "../../../../infra/database";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1;");
+  res.status(200).json({ data: result.rows });
 }
 
 export default status;
