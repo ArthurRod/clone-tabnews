70e64ce93c23f5643012fed1aa976231441084bb
Arthur Rodrigues <arthur9713@hotmail.com>
2025-02-28 17:47:27 -0300

    feat: finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,34 @@
 import database from "infra/database";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1;");
-  res.status(200).json({ data: result.rows });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue =
+    await databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    await databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const databaseOpenedConnectionsValue =
+    await databaseOpenedConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,13 @@
 test("GET to /api/v1/status should return status 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
-
   expect(res.status).toBe(200);
+
+  const resBody = await res.json();
+
+  const parsedUpdateAt = new Date(resBody.update_at).toISOString();
+  expect(resBody.update_at).toEqual(parsedUpdateAt);
+
+  expect(resBody.dependencies.database.version).toEqual("16.0");
+  expect(resBody.dependencies.database.max_connections).toEqual(100);
+  expect(resBody.dependencies.database.opened_connections).toEqual(1);
 });
